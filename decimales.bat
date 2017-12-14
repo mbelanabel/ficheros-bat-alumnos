d42fa947aded381b0a796b71389548a059d4d904
jtondadi <32275181+jtondadi@users.noreply.github.com>
2017-12-14 23:33:23 +0100

    Add files via upload

---
+++ decimales.bat
@@ -0,0 +1,33 @@
+﻿# http://www.tuwebdeinformatica.com/foro/viewtopic.php?f=41&t=2545
+
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+set /p d=Dividendo: 
+set /p di=Divisor: 
+set /p decim=Decimales: 
+set /a try=0
+set /a set=0
+set /a final=0
+set /a num1=%d%/%di%
+set resultado=echo Resultado %d%/%di% = %num1%.
+set /a res=%d%-(%di%*(%d%/%di%))
+:seteando
+if %set%==%decim% goto bucle
+set /a set=%set%+1
+set /a dec%set%=0
+goto seteando
+:bucle
+if %try%==%decim% goto fin
+set /a try=%try%+1
+set /a dec%try%=%res%0/%di%
+set /a p=%res%0/%di%
+set /a res=%res%0-(%di%*(%res%0/%di%))
+goto bucle
+:fin
+if %final%==%decim% goto resultado
+set /a final=%final%+1
+set resultado=%resultado%!dec%final%!
+goto fin
+:resultado
+%resultado%
+pause>nul
---
+++ puertos_firewall.bat
@@ -0,0 +1,31 @@
+@echo off
+rem ***
+rem JT. Diciembre 2014
+rem abrir rango de puertos para firewall de Windows
+rem 
+rem Variables
+rem incial=rango de puertos (primero)
+rem final=rango de puertos (ultimo)
+rem puertos 20 (datos) y 21 (control) -> colocados manualmente
+rem 
+rem ***
+
+
+set /a inicial=5001
+set /a final=5008
+set /a puerto=%inicial%
+
+:bucle
+rem echo %puerto%
+
+set /a puerto=%puerto%+1
+if %puerto% GTR %final% goto final
+echo ** PUERTO PASIVO %puerto% **
+netsh firewall add portopening protocol=TCP port=%puerto% name="FTP PASIVO %puerto%" > null
+goto bucle
+:final
+
+echo "puerto 21 control"
+netsh firewall add portopening protocol=TCP port=21 name="FTP control" > null
+echo "puerto 20 datos"
+netsh firewall add portopening protocol=TCP port=20 name="FTP datos" > null
